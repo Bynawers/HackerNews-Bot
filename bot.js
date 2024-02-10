c072caec39595459e5fa396b9a0e78297c9c7f61
Bynawers <bynawers@localhost.localdomain>
2024-02-10 12:15:16 +0100

    Automatic data request

--- bot.js
+++ bot.js
@@ -2,37 +2,37 @@ const Discord = require("discord.js");
 const { EmbedBuilder } = require("discord.js");
 const { Client, GatewayIntentBits } = require("discord.js");
 const axios = require("axios");
+const he = require("he");
 const { parseString } = require("xml2js");
+const fs = require("fs");
+
+const config = require("./config.json");
+
+let newsPosted = [];
+
+const MAX_NEWS_PER_POST = 3;
+const REQUEST_INTERVAL_PER_MINUTES = 60;
+const DATA_PATH = "data.json";
+const MAX_DATA_SAVED = 50;
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-/*
-const client = new Discord.Client({
-  intents: [
-    "GUILDS",
-    "GUILD_MESSAGES",
-    "GUILD_MESSAGE_REACTIONS",
-    "GUILD_MESSAGE_TYPING",
-    "DIRECT_MESSAGES",
-    "DIRECT_MESSAGE_REACTIONS",
-    "DIRECT_MESSAGE_TYPING",
-  ],
-  partials: ["CHANNEL"],
-});*/
-
-const config = require("./config.json");
-
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  //news();
-  fetchHackerNewData();
+
+  newsPosted = readData();
+
+  const intervalInMilliseconds = REQUEST_INTERVAL_PER_MINUTES * 60 * 1000;
+  setInterval(() => {
+    fetchHackerNewData();
+  }, intervalInMilliseconds);
 });
 
 const fetchHackerNewData = () => {
   axios
-    .get("https://feeds.feedburner.com/TheHackersNews")
+    .get("https://feeds.feedburner.com/TheHackersNews?max-results=4")
     .then((response) => {
       parseString(response.data, (error, result) => {
         if (error) {
@@ -40,7 +40,7 @@ const fetchHackerNewData = () => {
           return;
         }
         const channel = result.rss.channel[0];
-        console.log(channel.item);
+        checkAlreadyPosted(channel.item);
       });
     })
     .catch((error) => {
@@ -48,25 +48,66 @@ const fetchHackerNewData = () => {
     });
 };
 
-const news = () => {
+const checkAlreadyPosted = (request) => {
+  const isNewElement = (news) => {
+    return !newsPosted.some((elem) => {
+      return news.title[0] === elem.title[0];
+    });
+  };
+  postAllNews(request.filter(isNewElement));
+};
+
+const postAllNews = (data) => {
+  let i = 0;
+  console.log(data.length);
+  data.forEach((news) => {
+    if (i < MAX_NEWS_PER_POST) {
+      postNews(news);
+      newsPosted.unshift(news);
+      if (newsPosted.length > MAX_DATA_SAVED) {
+        newsPosted.shift();
+      }
+      i++;
+    }
+  });
+  saveData(newsPosted);
+};
+
+const postNews = (news) => {
   const channelId = config.channel_id;
 
   const channel = client.channels.cache.get(channelId);
 
+  const description = he.decode(news.description[0]);
+
   const embed = new EmbedBuilder()
-    .setColor(0x0099ff)
-    .setTitle("Hacking News !")
-    .setDescription("Some description here")
+    .setColor(0xb20000)
+    .setTitle(news.title[0])
+    .setURL(news.link[0])
+    .setDescription(description)
     .setTimestamp()
+    .setThumbnail(news.enclosure[0].$.url)
     .setFooter({
-      text: "Some footer text here",
-      iconURL: "https://i.imgur.com/AfFp7pu.png",
+      text: "The Hacker News",
+      iconURL:
+        "https://pbs.twimg.com/profile_images/1597647879811657728/FLgHrLHy_400x400.jpg",
     });
   if (channel == undefined) {
     console.log("Channel ID not find");
     return;
   }
   channel.send({ embeds: [embed] });
+};
+
+const saveData = (data) => {
+  const jsonData = JSON.stringify(data, null, 2);
+  fs.writeFileSync(DATA_PATH, jsonData);
+};
+
+const readData = () => {
+  const fileContent = fs.readFileSync(DATA_PATH, "utf-8");
+  const data = JSON.parse(fileContent);
+  return data;
 };
 
 client.login(config.discord_bot_token);
